446f446d88bb71398e2f3b7279093e451e6caae3
Daniel <daniel15006367@gmail.com>
2022-02-03 13:01:03 -0600

    modificaciones

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # frontend_rd
+Distribución del proyecto:
+
+- /
+-frontend.html
+- utilerías.js
+- LICENSE
+- README.md
+
+Pequeña aplicación para el ejercicio de:
+
+Ghibli films
+Crea una aplicación que liste y permita buscar películas de estudio Ghibli, el buscador debe soportar autocompletado.
+
+Se usó para el desarrollo, librerías de jquery y datatable además del uso de la librería autocomplete.js@10.2.6 (https://tarekraafat.github.io/autoComplete.js/demo/) para la funcionalidad de la búsqueda autocompletada
+
+------Detalles técnicos----
+Se baja la información de la URL del api Ghibli, y se pasa la información para el componente de autocompletado, la búsqueda es en relación a los títulos, 
+se usa el mismo data para posteriormente filtrarlo y colocarlo en el datatable final. El máximo de registros listados en el auto complementado es de 50.
--- utilerias.js
+++ utilerias.js
@@ -1,5 +1,8 @@
+
 	var tabla=null;
-		
+	var arrayReturn = [];
+	var dataset;
+	
 	function assignToEventsColumns(data) {
 		
 		if(tabla!==null){tabla.destroy();}
@@ -19,29 +22,22 @@
 	
 	function get_from_ws(filtro)
 	{
+		
+		
 		var arrayReturn = [];
 		filtro=filtro.toUpperCase();
-		$.ajax({
-				type: 'GET',
-				url: 'https://ghibliapi.herokuapp.com/films/',
-				dataType : "json",
-				success: function(data) {
-						title="";
-						$.each(
-							data,
-							function(index, arrayValores) {	
-								title=arrayValores.title.toUpperCase();
-								if( title.match(filtro) )
-								{										
-									arrayReturn.push(arrayValores);
-								}
-							}
-						);
-						console.log(data);
-						console.log(arrayReturn);
-						assignToEventsColumns(arrayReturn);
+		
+		$.each(
+			dataset,
+			function(index, arrayValores) {	
+				title=arrayValores.title.toUpperCase();
+				if( title.match(filtro) )
+				{										
+					arrayReturn.push(arrayValores);
 				}
-		});
+			}
+		);
+		assignToEventsColumns(arrayReturn);		
 	}
 	
 	function listar(e)
@@ -63,32 +59,35 @@
 			dataType : "json",
 			success: function(data) {
 					
+					dataset=data;
 					$.each(
 						data,
 						function(index, arrayValores) {
 							arrayReturn.push(arrayValores.title);
 						}
-					);
-					
+					);					
 					
 					const autoCompleteJS = new autoComplete({
-					placeHolder: "Agrega intro para buscar",
-					data: {
-						src: arrayReturn,
-						cache: true,
-					},
-					resultItem: {
-						highlight: true
-					},
-					events: {
-						input: {
-							selection: (event) => {								
-								const selection = event.detail.selection.value;
-								autoCompleteJS.input.value = selection;
+						placeHolder: "Agrega intro para buscar",
+						resultsList: {
+							maxResults: 50
+						},
+						data: {
+							src: arrayReturn,
+							cache: true,
+						},
+						resultItem: {
+							highlight: true
+						},
+						events: {
+							input: {
+								selection: (event) => {								
+									const selection = event.detail.selection.value;
+									autoCompleteJS.input.value = selection;
+								}
 							}
 						}
-					}
-				});
+					});
 				
 			}
 	});
